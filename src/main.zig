b7f46783ed5fb857990dba5c562443a10b44bc48
kaleab <808kaleabkzin@gmail.com>
2024-03-07 23:21:23 +0300

    timing

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 const Display = @import("display.zig").Display;
 const Bitmap = @import("bitmap.zig").Bitmap;
 const Device = @import("device.zig").Device;
+const CPU = @import("cpu.zig").CPU;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -24,13 +25,28 @@ pub fn main() !void {
 
     var bitmap = try Bitmap.create(allocator, 64, 32);
     defer bitmap.free();
-    _ = bitmap.setPixel(5, 5);
+    // _ = bitmap.setPixel(5, 5);
 
     var display = try Display.create("CHIP-8", 600, 300, bitmap.width, bitmap.height);
     defer display.free();
 
+    var cpu = CPU.create(&device.memory, &bitmap, &display);
+
+    const fps: f32 = 60.0;
+    const fpsInterval = 1000.0 / fps;
+    var previousTime = std.time.milliTimestamp();
+    var currentTime = std.time.milliTimestamp();
+
     while (display.open) {
         display.input();
-        display.draw(&bitmap);
+
+        currentTime = std.time.milliTimestamp();
+        if (@as(f32, @floatFromInt(currentTime - previousTime)) > fpsInterval) {
+            previousTime = currentTime;
+
+            cpu.cycle();
+
+            display.draw(&bitmap);
+        }
     }
 }
